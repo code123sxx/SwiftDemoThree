8ff2c93f50eed30f36f077ec83ea233bd5acf614
尚一杰 <2015089414@qq.com>
2014-06-04 21:24:41 +0800

    Initial Commit

---
+++ SwiftDemoThree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDemoThree
+//
+//  Created by shangyijie on 14/6/4.
+//  Copyright (c) 2014年 syj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
